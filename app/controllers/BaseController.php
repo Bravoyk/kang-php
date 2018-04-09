6f0fe680e584554f334d758a86c466fe88f46780
Bravo <wispmagical@gmail.com>
2018-04-10 00:40:17 +0800

    update composer and app

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bravo
+ * Date: 2018/4/10
+ * Time: 12:19 AM
+ */
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bravo
+ * Date: 2018/4/8
+ * Time: 10:54 PM
+ */
