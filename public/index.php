997a04b17970cf020150ad458fe741383f5c838d
Bravo <wispmagical@gmail.com>
2019-04-15 16:03:29 +0800

    init

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/*************************************************
+ *                  Kang PHP                     *
+ *                                               *
+ * A Faster Lightweight Full-Stack PHP Framework *
+ *                                               *
+ *                  Bravo                        *
+ *        <https://github.com/Bravo>             *
+ *                                               *
+ *              Version: 0.0.1                   *
+ *************************************************/
+
+// 载入框架运行文件
+echo 1;
+//require(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'framework/run.php');
